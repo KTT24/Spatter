bcd76008835a56b3f2b6f1f15726ca48799eadee
Kutter Thornton <kutter.thornton@gmail.com>
2023-04-20 16:41:30 -0500

    🛠️ Update Website

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+
+<template>
+  <body>
+    <div>
+      <h1 class="CommingSoon">
+        Comming Soon
+      </h1>
+      <h3 class="subHeading">
+        May 12 2023
+      </h3>
+    </div>
+  </body>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
