02577a5eb290221f01be51aa2e6f6ee81e6eb3da
Kutter Thornton <kutter.thornton@gmail.com>
2023-05-18 09:02:33 -0500

    Fix CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url"
 
 export default {
-    buildModules: [
-        '@nuxtjs/style-resources',
-        '@nuxtjs/vite',
-        '@nuxtjs/google-fonts'
-    ],
-    css: [
-        '~/assets/style/style.scss'
-    ],
+  buildModules: ["@nuxtjs/style-resources", "@nuxtjs/vite", "@nuxtjs/google-fonts"],
+  css: ["~/assets/style/style.scss", "~/assets/style/style.css"],
 }
